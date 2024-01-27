4fc849ee649fa441faedab3c7341f35634a2db58
rmkane <rmkane89@gmail.com>
2024-01-27 15:21:27 -0500

    Initialize frontend

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+
+// Source:
+// https://www.robinwieruch.de/vitest-react-testing-library/
+
+describe("something truthy and falsy", () => {
+  it("true to be true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("false to be false", () => {
+    expect(false).toBe(false);
+  });
+});
